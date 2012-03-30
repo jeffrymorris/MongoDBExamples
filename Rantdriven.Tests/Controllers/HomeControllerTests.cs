ba84423e043f7c30d4b3779bccd03737f5ecc96a
jmorris <jeffrymorris@gmail.com>
2012-03-29 21:30:59 -0700

    initial

---
+++ Rantdriven.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Rantdriven.Controllers;
+
+namespace Rantdriven.Tests.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        [Test]
+        public void Setup()
+        {
+        }
+
+        public void TestIndex()
+        {
+            var controller = new HomeController();
+            var view = controller.Index();
+            
+            
+        }
+
+        [Test]
+        public void TearDown()
+        {
+            
+        }
+    }
+}
