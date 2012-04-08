15e45e09cc3ec2f6ee1f52f586cba55d226b4e55
jmorris <jeffrymorris@gmail.com>
2012-04-07 20:42:16 -0700

    few changes

---
+++ Rantdriven.Core/Bootstrap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace Rantdriven.Core
+{
+    public class Bootstrap
+    {
+        public static void Main(IMongoApplication application)
+        {
+            var connectionString = ConfigurationManager.AppSettings["MongoDb"];
+            application.Mongod = MongoServer.Create(connectionString);
+        }
+    }
+}
---
+++ Rantdriven.Core/IMongoApplication.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace Rantdriven.Core
+{
+    public interface IMongoApplication
+    {
+        MongoServer Mongod { get; set; }
+    }
+}
